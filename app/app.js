1e6c2a8dad3a1da59ae6465482630aaaea74a0af
Shawak <maxi.nussbaum@googlemail.com>
2017-03-15 12:18:55 +0100

    optimazions and enemy layout

--- app/app.js
+++ app/app.js
@@ -10,15 +10,128 @@
  viewport
  http://stackoverflow.com/questions/16919601/html5-canvas-camera-viewport-how-to-actally-do-it ?
 
+ zooming in/out relative to the mouse position
+ http://stackoverflow.com/questions/2916081/zoom-in-on-a-point-using-scale-and-translate
 
  */
+
+var resources = {
+
+    count: 0,
+    loaded: 0,
+
+    map: {},
+
+    add: function(name, src) {
+        var img = new Image();
+        img.src = src;
+        img.onload = function() { resources.loaded++; };
+        resources.count++;
+        resources.map[name] = img;
+    },
+
+    get: function(name) {
+        return resources.map[name];
+    }
+
+};
+
+var camera = {
+
+    ratio: 1,
+    zoom: 1,
+    view: {x: 0, y: 0},
+
+    // original screen size
+    origin: {width: 0, height: 0},
+
+    init: function() {
+
+        if(true) {
+            // high definition canvas
+            var dpr = window.devicePixelRatio || 1;
+            var bsr = app.ctx.webkitBackingStorePixelRatio ||
+                app.ctx.mozBackingStorePixelRatio ||
+                app.ctx.msBackingStorePixelRatio ||
+                app.ctx.oBackingStorePixelRatio ||
+                app.ctx.backingStorePixelRatio || 1;
+
+            var ratio = dpr / bsr;
+
+            var oldWidth = app.canvas.width;
+            var oldHeight = app.canvas.height;
+            app.canvas.width = oldWidth * ratio;
+            app.canvas.height = oldHeight * ratio;
+            app.canvas.style.width = oldWidth + 'px';
+            app.canvas.style.height = oldHeight + 'px';
+
+            camera.ratio = ratio;
+        }
+
+        camera.origin.width = app.canvas.width;
+        camera.origin.height = app.canvas.height;
+    },
+
+    transform: function(ctx) {
+        app.ctx.scale(camera.ratio, camera.ratio);
+        ctx.setTransform(camera.zoom, 0, 0, camera.zoom, -camera.view.x, -camera.view.y);
+    },
+
+    screenToWorld: function(point) {
+        return {
+            x: (point.x + camera.view.x) / camera.zoom,
+            y: (point.y + camera.view.y) / camera.zoom
+        };
+    },
+
+    /*worldToScreen: function (point) {
+        return {
+            x: point.x / camera.ratio,
+            y: point.y / camera.ratio
+        };
+    }*/
+
+    screenToCanvas: function(vector) {
+        return {
+            x: vector.x / camera.ratio,
+            y: vector.y / camera.ratio
+        };
+    }
+
+};
+
+var engine = {
+
+    drawText: function(text, pos, font, align, color) {
+        app.ctx.font = font || fonts.info;
+        app.ctx.textAlign = align || 'start';
+        app.ctx.fillStyle = color || 'white';
+        app.ctx.fillText(text, pos.x, pos.y);
+    },
+
+    drawImage: function(img, pos) {
+        app.ctx.drawImage(img, pos.x, pos.y);
+    }
+
+};
+
+var fonts = {
+    info: '10px sans-serif',
+    playerInfo: '20px sans-serif'
+};
+
+var align = {
+    start: 'start',
+    end: 'end',
+    left: 'left',
+    center: 'center',
+    right: 'right'
+};
 
 var app = {
 
     ctx: null,
     mouseIsDown: false,
-    zoom: 1,
-    view: {x: 0, y: 0},
     cursor: {x: 0, y: 0},
     dragOffset: {x: 0, y: 0},
 
@@ -40,32 +153,14 @@ var app = {
         app.canvas.width = window.innerWidth;
         app.canvas.height = window.innerHeight;
 
-        // high definition canvas
-        var dpr = window.devicePixelRatio || 1;
-        var bsr = app.ctx.webkitBackingStorePixelRatio ||
-            app.ctx.mozBackingStorePixelRatio ||
-            app.ctx.msBackingStorePixelRatio ||
-            app.ctx.oBackingStorePixelRatio ||
-            app.ctx.backingStorePixelRatio || 1;
-
-        var ratio = dpr / bsr;
-
-        var oldWidth = app.canvas.width;
-        var oldHeight = app.canvas.height;
-        app.canvas.width = oldWidth * ratio;
-        app.canvas.height = oldHeight * ratio;
-        app.canvas.style.width = oldWidth + 'px';
-        app.canvas.style.height = oldHeight + 'px';
-
-        app.ratio = ratio;
-        app.ctx.scale(ratio, ratio);
+        camera.init();
 
 
         // track events
         app.canvas.onmousedown = function (e) {
             app.mouseIsDown = true;
-            app.dragOffset.x = e.x - app.view.x;
-            app.dragOffset.y = e.y - app.view.y;
+            app.dragOffset.x = -e.pageX * camera.ratio - camera.view.x;
+            app.dragOffset.y = -e.pageY * camera.ratio - camera.view.y;
         };
 
         app.canvas.onmouseup = function (e) {
@@ -73,50 +168,58 @@ var app = {
         };
 
         app.canvas.onmousemove = function (e) {
-            app.cursor.x = e.clientX;
-            app.cursor.y = e.clientY;
+            var cursor = camera.screenToWorld({x: e.clientX, y: e.clientY});
+            app.cursor.x = cursor.x;
+            app.cursor.y = cursor.y;
 
             if (!app.mouseIsDown) {
                 return;
             }
 
-            app.view.x = e.x - app.dragOffset.x;
-            app.view.y = e.y - app.dragOffset.y;
+            camera.view.x = -e.pageX * camera.ratio - app.dragOffset.x;
+            camera.view.y = -e.pageY * camera.ratio - app.dragOffset.y;
         };
 
-        app.canvas.onmousewheel = function (e) {
+        app.canvas.onwheel = function (e) {
             e.preventDefault();
 
-            var diff = -e.deltaY * 0.1 / 640;
+            var oldZoom = camera.zoom;
 
-            if (app.zoom + diff < 0.1) {
-                diff = 0.1 - app.zoom;
-            } else if (app.zoom + diff > 1) {
-                diff = 1 - app.zoom;
+            var wheel = e.deltaY > 0 ? -1 : 1;
+            var diff = Math.exp(wheel * 0.1);
+            var newZoom = camera.zoom * diff;
+            if(newZoom <= 0.01 || newZoom >= 1) {
+                return;
             }
 
-            app.zoom += diff;
+            camera.zoom = newZoom;
 
             // TODO
             // move camera view depending on the current view and the zoom diff
             // to keep the camera centering
+            // current method doesn't work perfectly/flawless
+
+            var scale = 1 / oldZoom;
+            var mouse = {
+                x: e.clientX * camera.ratio - camera.view.x,
+                y: e.clientY * camera.ratio - camera.view.y
+            };
+
+            camera.view.x -= mouse.x / (scale * diff) - mouse.x / scale;
+            camera.view.Y -= mouse.y / (scale * diff) - mouse.y / scale;
         };
 
-        app.view.x = app.canvas.height / 2;
-        app.view.y = 50;
-        app.zoom = 0.2;
+        camera.view.x = -300;
+        camera.view.y = -20;
+        camera.zoom = 0.2;
 
         app.load();
         app.start();
     },
 
     load: function () {
-        app.resources['background'] = new Image();
-        app.resources['background'].src = 'monopvply.jpg';
-        app.resources['background'].onload = function () {
-            app.resourcesLoaded++;
-        };
-        app.resourcesCount++;
+        resources.add('enemy', 'https://placeholdit.imgix.net/~text?txtsize=14&txt=Enemy&w=100&h=100');
+        resources.add('background', 'monopvply.jpg');
     },
 
     start: function () {
@@ -155,25 +258,47 @@ var app = {
     },
 
     draw: function (interp) {
+        // reset
         app.ctx.fillStyle = 'rgb(29,29,29)';
         app.ctx.fillRect(0, 0, app.canvas.width, app.canvas.height);
 
-        app.ctx.save();
-
-        //app.ctx.scale(app.scale, app.scale);
-        app.ctx.setTransform(app.zoom, 0, 0, app.zoom, app.view.x * app.ratio, app.view.y * app.ratio);
-
-        if (app.resourcesLoaded != app.resourcesCount) {
-            app.ctx.fillText('Loading..', 10, 10);
+        if (resources.loaded != resources.count) {
+            engine.drawText('Loading..', {x: app.canvas.width / 2, y: app.canvas.height / 2});
             return;
         }
 
-        app.ctx.drawImage(app.resources['background'], 0, 0);
+        // start of map drawing
+        app.ctx.save();
+        camera.transform(app.ctx);
 
-        // information
+        // drawing map
+
+        engine.drawImage(resources.get('background'), {x: 0, y: 0});
+
+        app.ctx.beginPath();
+        app.ctx.fillStyle = 'red';
+        app.ctx.arc(2925, 2737.5, 10, 0, 2 * Math.PI, false);
+        app.ctx.fill();
+        app.ctx.stroke();
+
+        // end of map drawing
         app.ctx.restore();
 
+        // game information
+        var playerCount = 4;
+        var offsetY = (app.canvas.height - playerCount * 110) / 2;
+        for(var i = 0; i < playerCount; i++) {
+            var pos = {x: app.canvas.width - 100, y: offsetY + 110 * i};
+            engine.drawImage(resources.get('enemy'), pos);
+            engine.drawText('Enemy', {x: pos.x - 10, y: pos.y + 16}, fonts.playerInfo, align.right);
+            engine.drawText('8673 $', {x: pos.x - 10, y: pos.y + 56}, fonts.playerInfo, align.right);
+            engine.drawText('5 H', {x: pos.x - 10, y: pos.y + 76}, fonts.playerInfo, align.right);
+        }
+
+        // general information
         app.ctx.fillStyle = 'rgb(220, 220, 220)';
+        app.ctx.font = fonts.info;
+        app.ctx.textAlign = 'start';
 
         app.ctx.beginPath();
         app.ctx.moveTo(0, 0);
@@ -186,26 +311,30 @@ var app = {
         var info = [
             'Resources: ' + app.resourcesLoaded + '/' + app.resourcesCount,
             'Canvas: ' + app.canvas.width + 'x' + app.canvas.height,
-            'Ratio: ' + app.ratio,
-            'Zoom: ' + app.zoom,
-            'View: x ' + Math.round(app.view.x) + ' y ' + Math.round(app.view.y),
+            'Camera',
+            '  Ratio: ' + camera.ratio,
+            '  Zoom: ' + camera.zoom,
+            '  View: x ' + Math.round(camera.view.x) + ' y ' + Math.round(camera.view.y),
             'Cursor: x ' + Math.round(app.cursor.x) + ' y ' + Math.round(app.cursor.y),
             'FPS: ' + Math.round(app.fps)
         ];
 
         for(var i = 0; i < info.length; i++) {
-            app.ctx.fillText(info[i], 35, (i + 3) * 10)
+            app.ctx.fillText(info[i], 45, (i + 4) * 10)
         }
 
-        i = 0;
-        while (i <= 20 * 100) {
-            app.ctx.fillText(i, i != 0 ? i : 4, 10);
-            app.ctx.fillText(i, 4, i);
-            i += 100;
+        for(var i = 1; i < Math.max(app.canvas.width, app.canvas.height) / 100; i++) {
+            var offset = i * 100;
+            var world = camera.screenToWorld({x: offset, y: offset});
+            app.ctx.fillText(offset.toString(), offset != 0 ? offset : 4, 10);
+            app.ctx.fillText(offset.toString(), 4, offset);
+            app.ctx.fillText(Math.round(world.x).toString(), offset != 0 ? offset : 4, 20);
+            app.ctx.fillText(Math.round(world.y).toString(), 4, offset + 10);
         }
     }
 
 };
 (function () {
+    window.applicationCache.update();
     app.init();
 })();
