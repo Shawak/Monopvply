b8712fef15ce51d985b34b2d594bb8e5d2800fbe
epvpsyc <dev@sycophantic.de>
2017-03-17 00:09:03 +0100

    changed .png to .jpg

--- frontend/js/predefinedMaps.js
+++ frontend/js/predefinedMaps.js
@@ -1,50 +1,50 @@
 
 function setUpStandardMap(gameMap)
 {
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "brown", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "", "", 0 ));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "brown", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "brown", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "", "", 0 ));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "brown", "Browser Games", 500));
 	
-	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.png", "", "", 400));
-	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.png", "", "Hafen", 400));
+	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.jpg", "", "", 400));
+	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.jpg", "", "Hafen", 400));
 	
-	gameMap.addSideField(new Field("./img/streets/4_nostale.png", "lightblue", "Nostale", 220));
+	gameMap.addSideField(new Field("./img/streets/4_nostale.jpg", "lightblue", "Nostale", 220));
 	
-	gameMap.addSideField(new Field("./img/streets/5_last_chaos.png", "", "", 300));
+	gameMap.addSideField(new Field("./img/streets/5_last_chaos.jpg", "", "", 300));
 	
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "lightblue", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "lightblue", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "lightblue", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "lightblue", "Console Games", 120));
 	
 	
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "green", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "green", "Console Games", 120));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "green", "Browser Games", 500));
-	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.png", "red", "Final Fantasy", 400));
-	gameMap.addSideField(new Field("./img/streets/4_nostale.png", "red", "Nostale", 220));
-	gameMap.addSideField(new Field("./img/streets/5_last_chaos.png", "red", "Last Chaos", 130));
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "green", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "green", "Console Games", 120));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "green", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "green", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "green", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "green", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.jpg", "red", "Final Fantasy", 400));
+	gameMap.addSideField(new Field("./img/streets/4_nostale.jpg", "red", "Nostale", 220));
+	gameMap.addSideField(new Field("./img/streets/5_last_chaos.jpg", "red", "Last Chaos", 130));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "green", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "green", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "green", "Browser Games", 500));
 	
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "green", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "green", "Console Games", 120));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "green", "Browser Games", 500));
-	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.png", "red", "Final Fantasy", 400));
-	gameMap.addSideField(new Field("./img/streets/4_nostale.png", "red", "Nostale", 220));
-	gameMap.addSideField(new Field("./img/streets/5_last_chaos.png", "red", "Last Chaos", 130));
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "green", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "green", "Console Games", 120));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "green", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "green", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "green", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "green", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.jpg", "red", "Final Fantasy", 400));
+	gameMap.addSideField(new Field("./img/streets/4_nostale.jpg", "red", "Nostale", 220));
+	gameMap.addSideField(new Field("./img/streets/5_last_chaos.jpg", "red", "Last Chaos", 130));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "green", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "green", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "green", "Browser Games", 500));
 	
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "green", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "green", "Console Games", 120));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "green", "Browser Games", 500));
-	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.png", "red", "Final Fantasy", 400));
-	gameMap.addSideField(new Field("./img/streets/4_nostale.png", "red", "Nostale", 220));
-	gameMap.addSideField(new Field("./img/streets/5_last_chaos.png", "red", "Last Chaos", 130));
-	gameMap.addSideField(new Field("./img/streets/1_general_gaming.png", "green", "General Gaming", 100));
-	gameMap.addSideField(new Field("./img/streets/2_consoles.png", "green", "Console Games", 120));
-	gameMap.addSideField(new Field("./img/streets/3_browser_games.png", "green", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "green", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "green", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "green", "Browser Games", 500));
+	gameMap.addSideField(new Field("./img/streets/4_final_fantasy.jpg", "red", "Final Fantasy", 400));
+	gameMap.addSideField(new Field("./img/streets/4_nostale.jpg", "red", "Nostale", 220));
+	gameMap.addSideField(new Field("./img/streets/5_last_chaos.jpg", "red", "Last Chaos", 130));
+	gameMap.addSideField(new Field("./img/streets/1_general_gaming.jpg", "green", "General Gaming", 100));
+	gameMap.addSideField(new Field("./img/streets/2_consoles.jpg", "green", "Console Games", 120));
+	gameMap.addSideField(new Field("./img/streets/3_browser_games.jpg", "green", "Browser Games", 500));
 	
 	gameMap.addCornerField(new Field("./img/corner.jpg", "", "Let's go!", 0,""));
 	gameMap.addCornerField(new Field("./img/corner.jpg", "", "Dead", 0,""));
