eb5a87fecbea44b41ff6d93b32f0ffd9e0e5a235
Shawak <maxi.nussbaum@googlemail.com>
2017-03-16 22:50:26 +0100

    small fixes

--- public/js/client.js
+++ public/js/client.js
@@ -7,9 +7,7 @@ function Client() {
 
     this.socket.on('connect', function () {
         console.log('connect');
-        var packet = new window.exports.loginPacket('Shawak', 'test');
-        console.log(packet);
-        self.sendPacket(packet);
+        self.sendPacket(new loginPacket('Shawak', 'test'));
     });
 
     this.socket.on('packet', function (data) {
@@ -21,12 +19,16 @@ function Client() {
     });
 
     this.sendPacket = function(packet) {
-        this.socket.emit('packet', window.exports.packetManager.pack(packet));
+        this.socket.emit('packet', packetManager.pack(packet));
     };
 
 }
 
 (function () {
+
+    for(var prop in window.exports) {
+        window[prop] = window.exports[prop];
+    }
 
     new Client();
 
