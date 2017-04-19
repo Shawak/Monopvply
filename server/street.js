0812acaaa138096d230cf55f058549f14a1c4945
Shadow992 <m.rupprecht@gmx.net>
2017-04-19 18:02:49 +0200

    Minor Fixes

--- server/street.js
+++ server/street.js
@@ -2,12 +2,13 @@ const Field = require('./field.js');
 
 class Street extends Field {
 
-    constructor(id, name, img, group, color, price) {
+    constructor(id, name, img, group, color, price, textColor=undefined) {
         super(id, name, img);
 
         this.group = group;
         this.color = color;
         this.price = price;
+		this.textColor = textColor;
 
         this.owner = null;
         this.houses = null;
